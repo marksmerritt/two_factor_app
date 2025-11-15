97b1c8e0eab886861bda537ab9179298c22b492e
Mark Merritt <markmerritt804@gmail.com>
2025-11-15 18:03:23 -0500

    wip

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,10 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get home_index_url
+    # Sign in a user first since the controller requires authentication
+    user = users(:one)
+    sign_in user
+    get root_url
     assert_response :success
   end
 end
